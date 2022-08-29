096eb4a719921ec6a9c7c509d56196a1a6e7bb4e
imgurbot12 <imgurbot12@gmail.com>
2022-08-29 02:25:51 -0700

    feat: add executable mode to ewfi

--- config/eww/ewfi/scripts/search.sh
+++ config/eww/ewfi/scripts/search.sh
@@ -16,8 +16,14 @@ DIR_ICON="$IMG/dir-icon.png"
 #: file path icon
 FILE_ICON="$IMG/file-icon.png"
 
-#: command used to list files in directory
-LIST_DIR="ls"
+#: find command binary location
+FIND_CMD="/usr/bin/find"
+
+#: args used to list all files/dirs in directory
+FIND_ALL=" -maxdepth 1"
+
+#: args used to find only executables in directory
+FIND_EXEC="$FIND_ALL -executable"
 
 #: command to launch file browser
 LOAD_DIR="alacritty -e joshuto --path {}"
@@ -26,7 +32,7 @@ LOAD_DIR="alacritty -e joshuto --path {}"
 LOAD_FILE="alacritty -e nvim {}"
 
 #: max files allowed to load in single search
-MAX_FILES="20"
+MAX_FILES="30"
 
 #: quit sequence to close window
 QUIT_CMD=":q"
@@ -45,30 +51,32 @@ fmt_file () {
 }
 
 #: desc  => list icon to use for specified file 
-#: usage => ls $path | file_icon $path
-file_icon () {
-  b=`add_slash <<< "${1:-''}"`
-  for p in `</dev/stdin`; do
-    f="$b$p"
-    if [ ! -d "$f" ]; then
-      fmt_file "$f" "$FILE_ICON" "$(sed "s~{}~$f~g" <<< $LOAD_FILE)"
-      continue
-    fi
-    fmt_file "$f" "$DIR_ICON" "$(sed "s~{}~$f~g" <<< $LOAD_DIR)"
+#: usage => ls $path | file_result
+file_result () {
+  for f in `</dev/stdin`; do
+    i="$FILE_ICON"
+    c='{}'
+    [ -z "$EXEC_MODE" ] && c="$LOAD_FILE"
+    [ -d "$f" ] && i="$DIR_ICON" && [ -z "$EXEC_MODE" ] && c="$LOAD_DIR"
+    fmt_file "$f" "$i" "$(sed "s~{}~$f~g" <<< $c)"
   done
 }
 
 #: desc  => search filesystem files at the given path
 #: usage => search_files $search
 search_files () {
-  if [ ! -d "$1" ]; then
-    d=$(dirname $1)
+  f=`expand_user <<< "$1" | add_slash`
+  # determine find arguments
+  [[ $f = !* ]] && export EXEC_MODE="1" && f=${f:1} && a="$FIND_EXEC" || a="$FIND_ALL"
+  # complete search
+  if [ ! -d "$f" ]; then
+    d=$(dirname $f)
     [ ! -d "$d" ] && echo "EWFI: skipping search on invalid dir: $d" && return 0
-    s=$(basename $1)
-    $LIST_DIR "$d" | grep -i "^$s" | head -n $MAX_FILES | file_icon "$d"
+    s=$(basename $f)
+    $FIND_CMD "$d" $a -iname "$s*" | head -n $MAX_FILES | file_result 
     return 0
   fi
-  $LIST_DIR "$1" | head -n $MAX_FILES | file_icon "$1"
+  $FIND_CMD "$f" $a | head -n $MAX_FILES | file_result
 }
 
 #: desc  => search desktop apps for the given text
@@ -86,6 +94,9 @@ search_app_cache () {
 
 ARGS="$@"
 
+# search_files "$@"
+# exit 0
+
 # handle quit command
 if [ "$ARGS" == "$QUIT_CMD" ]; then
   echo "EWFI: executing quit command"
@@ -101,10 +112,9 @@ fi
 
 S="(box :class 'search-result-list' :orientation 'v' :space-evenly false "
 
-# process file searches
-if grep -qE "^[~\./]" <<< "$ARGS"; then
-  dir=`echo $@ | expand_user`
-  C=$(search_files "$dir")
+# process file/executable searches
+if grep -qE "^[!~\./]" <<< "$ARGS"; then
+  C=$(search_files "$@")
 else
   C=$(search_app_cache "$@")
 fi
