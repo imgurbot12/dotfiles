e9f0425c3d4c23f35f999db403ec9482d2c16f4c
imgurbot12 <imgurbot12@gmail.com>
2022-08-28 21:04:44 -0700

    feat: ewfi supports file searches

--- config/eww/ewfi/scripts/search.sh
+++ config/eww/ewfi/scripts/search.sh
@@ -2,26 +2,72 @@
 
 #** Variables **#
 
+IMG=`realpath "$(dirname $0)/../img"`
+
 #: eww executable path
 EWW="$HOME/.cargo/bin/eww"
 
 #: application results cache
 CACHE_FILE="$HOME/.cache/ewfi_elements"
 
+#: directory path icon
+DIR_ICON="$IMG/dir-icon.png"
+
+#: file path icon
+FILE_ICON="$IMG/file-icon.png"
+
+#: command used to list files in directory
+LIST_DIR="ls"
+
+#: command to launch file browser
+LOAD_DIR="alacritty -e joshuto --path {}"
+
+#: command to load file
+LOAD_FILE="alacritty -e nvim {}"
+
 #: quit sequence to close window
 QUIT_CMD=":q"
 
 #** Functions **#
 
+#: alias for tsv-parsing search-result generating awk command
+alias fmt_result="awk -F '\t' '{print \"(search_result :image \" \$4 \" :app \" \$1 \" :desc \" \$2 \" :cmd \" \$5 \" )\"}'"
+
+#: desc  => simplified fmt_result for formatting file entries
+#: usage => fmt_file $app $image $command
+fmt_file () {
+  echo -e "'$1'\t''\t''\t'$2'\t'$3'" | fmt_result
+}
+
+#: desc  => list icon to use for specified file 
+#: usage => ls $path | file_icon $path
+file_icon () {
+  b=`sed '/\/$/!s/$/\//' <<< "${1:-''}"`
+  for p in `</dev/stdin`; do
+    f="$b$p"
+    if [ ! -d "$f" ]; then
+      fmt_file "$f" "$FILE_ICON" "$(sed "s~{}~$f~g" <<< $LOAD_FILE)"
+      continue
+    fi
+    fmt_file "$f" "$DIR_ICON" "$(sed "s~{}~$f~g" <<< $LOAD_DIR)"
+  done
+}
+
+#: desc  => search filesystem files at the given path
+#: usage => search_files $search
+search_files () {
+  $LIST_DIR "$1" | file_icon "$1"
+}
+
 #: desc  => search desktop apps for the given text
-#: usage => search_apps $search
-search_cache () {
-  cat $CACHE_FILE    \
-  | cut -f1-3 \
-  | grep -in "$1"    \
-  | cut -d ':' -f1   \
+#: usage => search_app_cache $search
+search_app_cache () {
+  cat $CACHE_FILE  \
+  | cut -f1-3      \
+  | grep -in "$1"  \
+  | cut -d ':' -f1 \
   | xargs -I '{}' awk 'NR=={}{print;exit}' $CACHE_FILE \
-  | awk -F "\t" '{print "(search_result :image " $4 " :app " $1 " :desc " $2 " :cmd " $5 " )"}'
+  | fmt_result
 }
 
 #** Init **#
@@ -41,12 +87,20 @@ if grep -qE "^:.*" <<< "$ARGS"; then
   exit 0
 fi
 
+S="(box :class 'search-result-list' :orientation 'v' :space-evenly false "
 
-s="(box :class 'search-result-list' :orientation 'v' :space-evenly false "
-m=$(search_cache "$@")
-o="$s$m)"
+# process file searches
+if grep -qE "^[~\./]" <<< "$ARGS"; then
+  dir=`echo $@ | sed "s,~,$HOME,1"`
+  [ ! -d "$dir" ] && echo "skipping invalid dir '$dir'" && return 0
+  C=$(search_files "$dir")
+else
+  C=$(search_app_cache "$@")
+fi
 
-# echo -e $o
-eww update ewfi_elements="$o"
+O="$S$C)"
+
+# echo -e $O
+eww update ewfi_elements="$O"
 
 
