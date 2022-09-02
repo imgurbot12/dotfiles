d322f52a24ba77a02f0e7ecc8b78745f07a61a12
imgurbot12 <imgurbot12@gmail.com>
2022-09-01 19:15:39 -0700

    feat: workaround eww content literal size limits

--- config/eww/fbrowse/scripts/files.sh
+++ config/eww/fbrowse/scripts/files.sh
@@ -22,9 +22,6 @@ ARGS_HIDDEN="$ARGS -not -path '*/.*'"
 
 #** Functions **#
 
-#: functionify iconize script
-iconize="$(dirname $0)/icon.sh"
-
 #: usage => <cmd> | fmt_file
 fmt_file () {
   for f in `</dev/stdin`; do
@@ -39,11 +36,50 @@ list () {
   eval "$LIST_DIR '$1' $a | tail -n +2 " | fmt_file
 }
 
+#: usage => list_items $dir
+list_items () {
+  list "$1" | awk '{print "  (fbrowse_item :image " $1 " :path " $2 " :name " $3 ")"}'
+}
+
 #: usage => list_results $dir
 list_results () {
   echo "(box :class 'item-list' :orientation 'v'"
-  list "$1" | awk '{print "  (fbrowse_item :image " $1 " :path " $2 " :name " $3 ")"}'
-  echo ")"
+  list_items $1
+  echo ')'
+}
+
+#: usage => update_results $dir
+update_results () {
+  # refresh trackers
+  $EWW update \
+    fbrowse_path="$p"  \
+    fbrowse_files_1='' \
+    fbrowse_files_2='' \
+    fbrowse_files_3='' \
+    fbrowse_files_4='' \
+    fbrowse_files_5=''
+  # generate results in groups that eww can process
+  m=476
+  i=1
+  b=""
+  c=1
+  s="(box :class 'item-list' :orientation 'v' " 
+  e=")"
+  while IFS= read -r line; do
+    [ $i -eq 1 ] && b="$b$s"
+    b="$b  $line "
+    if [ $i -eq $m ]; then
+      i=0
+      # echo -e "$c $b$e"
+      $EWW update "fbrowse_files_$c"="$b$e"
+      b=""
+      c=`expr $c + 1`
+    fi
+    i=`expr $i + 1`
+  done <<< $(list "$1" | awk '{print "(fbrowse_item :image " $1 " :path " $2 " :name " $3 ")"}')
+  # echo -e "$c $b$e"
+  $EWW update "fbrowse_files_$c"="$b$e"
+  b="$b$e"
 }
 
 #** Init **#
@@ -58,8 +94,9 @@ case "$1" in
     list_results "$p"
     ;;
   "ur"|"update-results")
-    $EWW update fbrowse_path="$p"
-    $EWW update fbrowse_files="$(list_results $p)"
+    update_results "$p"
+    # $EWW update fbrowse_path="$p"
+    # $EWW update fbrowse_files="$(list_results $p)"
     ;;
   *)
     echo "invalid command. try <list/list-results/update-results>"
