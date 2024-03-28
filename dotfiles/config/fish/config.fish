f4b20b364e4f2a105dcf50b1e773ca40eac98cac
imgurbot12 <imgurbot12@gmail.com>
2024-03-28 00:25:17 -0700

    feat: rtx -> mise

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -8,7 +8,7 @@ set -x GOROOT     "/usr/local/go"
 set -x GOPATH     "$HOME/Code/golang"
 set -x CARGO_PATH "$HOME/.cargo"
 set -x PONY_PATH  "$HOME/.local/share/ponyup"
-set -x RTX_PATH   "$HOME/.local/share/rtx"
+set -x MISE_PATH  "$HOME/.local/share/mise"
 
 # omf installs
 set -x NVM_PATH   "$HOME/.nvm"
@@ -63,13 +63,13 @@ ensure_path "<builtin>" "$HOME/.local/bin"
 
 # omf => nvm / pyenv
 # ensure_omf "nvm"   "$NVM_PATH"
-# ensure_omf "pyenv" "$PYENV_PATH" 
+# ensure_omf "pyenv" "$PYENV_PATH"
 
 # path => golang / rust (cargo) / pony / pyenv
 ensure_path "go"    "$GOPATH/bin"     "$GOROOT/bin"
 ensure_path "cargo" "$CARGO_PATH/bin"
 ensure_path "ponyc" "$PONY_PATH/bin"
-ensure_path "rtx"   "$RTX_PATH/bin"
+ensure_path "mise"  "$MISE_PATH/bin"
 # ensure_path "pyenv" "$PYENV_PATH/bin"
 
 # command aliases
@@ -88,13 +88,13 @@ cmd_alias pbcopy  "xsel"  "-ib"
 cmd_alias pbcopy  "xclip" "-selection c"
 cmd_alias pbcopy  "wl-copy"
 cmd_alias pbpaste "xsel"  "-ob"
-cmd_alias pbpaste "xclip" "-selection c -o" 
+cmd_alias pbpaste "xclip" "-selection c -o"
 cmd_alias pbpaste "wl-paste"
 
 # interactive startup
 if status is-interactive
   if command -sq neofetch; neofetch; end
   if command -sq starship; eval (starship init fish); end
-  if command -sq rtx;      rtx activate -s fish | source; end
+  if command -sq mise;     mise activate -s fish | source; end
   if command -sq zoxide;   zoxide init fish --cmd cd | source; end
 end
--- install/mise.sh
+++ install/mise.sh
@@ -6,19 +6,19 @@
 #** Variables **#
 
 #: online script to install rtx multi-env language installer
-RTX="https://rtx.pub/install.sh"
+MISE_INSTALL="https://mise.run"
 
 #** Init **#
 
-# ensure bash is installed  
+# ensure bash is installed
 ensure_program bash
 
 # check if binary already exists
-if has_binary rtx; then 
-  log_info "rtx is already installed. updating installation..."
-  rtx self-update
+if has_binary mise; then
+  log_info "mise is already installed. updating installation..."
+  mise self-update
   exit 0
 fi
 
 # install rtx as standard
-curl -L $RTX | bash
+curl -L $MISE_INSTALL | bash
